7a5f5bddc5a9662700027dbb3aa16c4aad9ae8bf
ftctwistedmetal9433 <57970160+ftctwistedmetal9433@users.noreply.github.com>
2022-09-21 17:40:41 -0700

    Update Encoders.java

--- Encoders.java
+++ Encoders.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Encoder Auto", group="Robot")
-@Disabled
+
 public class Encoders extends LinearOpMode {
 
     /* Declare OpMode members. */
